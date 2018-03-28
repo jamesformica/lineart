bddfc0f6b2d9b02dec7ab284286f8a9cb2d0b103
James Formica <james.formica@gmail.com>
2018-03-28 16:48:46 +1100

    chore: add redux and css modules to prod

--- src/index.js
+++ src/index.js
@@ -7,7 +7,11 @@ import App from './App';
 import { reducer, initialState } from './reducer';
 import './index.css';
 
-const store = createStore(reducer, initialState);
+const store = createStore(
+  reducer,
+  initialState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   <Provider store={store}>
